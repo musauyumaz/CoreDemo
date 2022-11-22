80d78ab8168d6012f0a677d6d7dda6d90aa6eca2
musauyumaznotes <musauyumaznotes@gmail.com>
2022-11-22 10:22:48 +0300

    CORS Politikası

--- Presentation/MVC/Program.cs
+++ Presentation/MVC/Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddPersistenceServices();
+builder.Services.AddCors(options => options.AddDefaultPolicy(policy => policy.WithOrigins("https://localhost:7229,http://localhost:5229").AllowAnyHeader().AllowAnyMethod()));
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -14,6 +17,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseCors();
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
